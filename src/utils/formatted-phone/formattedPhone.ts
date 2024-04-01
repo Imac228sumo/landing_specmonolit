efa15fac18e5e4ec3db5caff521a45bbf7fa4264
Valery <valerkaogorodnikov@mail.ru>
2024-04-02 00:49:21 +0300

    redeploy_4

--- src/utils/formatted-phone/formattedPhone.ts
+++ src/utils/formatted-phone/formattedPhone.ts
@@ -1,5 +1,5 @@
 export const formattedPhone = (phone: string) => {
 	if (phone === '') return null
-	const formattedPhoneNumber = `+7(${phone.substring(1, 4)}) ${phone.substring(4, 7)} ${phone.substring(7)}`
+	const formattedPhoneNumber = `+7(${phone.substring(2, 5)}) ${phone.substring(5, 8)} ${phone.substring(8, 10)} ${phone.substring(10, 12)}`
 	return formattedPhoneNumber
 }
